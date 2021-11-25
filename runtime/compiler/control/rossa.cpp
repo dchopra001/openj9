989ab47617ca25f0e796ef750e8f6e18c82e9277
Rahil Shah <rahil@ca.ibm.com>
2021-11-25 15:42:48 -0500

    Add JNI Call to set ForceUsePreexistence
    Add a JNI method which can be used to set ForceUsePreexistence JIT
    options for DebugAgent.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- runtime/compiler/control/rossa.cpp
+++ runtime/compiler/control/rossa.cpp
@@ -1848,6 +1848,7 @@ onLoadInternal(
    jitConfig->debugAgentRevertToInterpreter = debugAgentRevertToInterpreter;
    jitConfig->debugAgentRecompile = debugAgentRecompile;
    jitConfig->debugAgentEnd = debugAgentEnd;
+   jitConfig->debugAgentSetForceUsePreexistence = debugAgentSetForceUsePreexistence;
    
    jitConfig->printAOTHeaderProcessorFeatures = printAOTHeaderProcessorFeatures;
 
